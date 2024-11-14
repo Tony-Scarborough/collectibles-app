bf4f428957069544c0b0994e9d11aa78a8c22e0e
Tony-Scarborough <tony@scarborough.gen.nz>
2024-11-15 12:35:55 +1300

    On branch main Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CollectiblesApp from './collectiblesApp';
+
+function App() {
+  return (
+    <div className="App">
+      <CollectiblesApp />
+    </div>
+  );
+}
+
+export default App;
